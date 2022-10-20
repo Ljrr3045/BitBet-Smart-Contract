0a920bedfda7b58a330939b40e3e86d37839c11d
Ljrr3045 <ljrr3045@gmail.com>
2022-10-20 14:37:04 -0400

    Use Access control

--- contracts/random number/RandomNumber.sol
+++ contracts/random number/RandomNumber.sol
@@ -10,10 +10,10 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  
-contract RandomNumber is VRFConsumerBase, Ownable{
+contract RandomNumber is VRFConsumerBase, AccessControl{
 
     using SafeMath for uint;
 
@@ -24,11 +24,21 @@ contract RandomNumber is VRFConsumerBase, Ownable{
     uint public until;
     ///@dev These are the global variables used for contract management.
 
+//Events
+
+    event newRandomNumber(uint indexed _newResult);
+    event newNumberLimit (uint indexed _until);
+    event withdrawContractFunds (uint indexed _time, bool _success);
+
     /**
         @notice The address of the VRF Coordinator contract must be supplied, depending on the blocksChain in which we are.
         @dev For both the keyHash and Fee variables, their value will be variable as long as different strings are used.
     */
-    constructor(address _vrfCoordinator, address _linkAddress) 
+    constructor(
+        address _vrfCoordinator, 
+        address _linkAddress,
+        address _adminContract
+        ) 
         VRFConsumerBase(
             _vrfCoordinator,
             _linkAddress 
@@ -36,15 +46,21 @@ contract RandomNumber is VRFConsumerBase, Ownable{
     {
         keyHash = 0xc251acd21ec4fb7f31bb8868288bfdbaeb4fbfec2df3735ddbd4f7dc8d60103c;
         fee = 2 * (10 ** 17);
-        until = 1;
+        until = 999999;
+
+        _grantRole("Admin", msg.sender);
+        _grantRole("Contract", _adminContract);
+        _setRoleAdmin("", "Admin");
     }
-    
+
+//Functions
+
     /**
         @notice This function allows you to request a random number 
         @dev For this function to work, you must previously supply this contract with Link Token, to cover 
         the corresponding fees. This function can only be called by the owner of the contract.
     */
-    function getRandomNumber() public onlyOwner returns(bytes32 requestId) {
+    function getRandomNumber() public onlyRole("Contract") returns(bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
         lastRequestId = requestRandomness(keyHash, fee);
         return lastRequestId;
@@ -53,6 +69,8 @@ contract RandomNumber is VRFConsumerBase, Ownable{
     ///@dev Internal function overridden to set the variable randomResult.
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         randomResult = randomness.mod(until).add(1);
+
+        emit newRandomNumber(randomResult);
     }
 
 //Utility Functions
@@ -62,8 +80,10 @@ contract RandomNumber is VRFConsumerBase, Ownable{
         numbers in which you want to obtain the random number, ex: 1 to 50.
         @dev This function can only be called by the owner of the contract.
     */
-    function setUntil(uint _until) public onlyOwner{
+    function setUntil(uint _until) public onlyRole("Admin"){
         until = _until;
+
+        emit newNumberLimit(until);
     }
 
     /**
@@ -72,7 +92,7 @@ contract RandomNumber is VRFConsumerBase, Ownable{
         @dev If you want the function to retrieve tokens as well, you must provide the address of the token 
         and set _withdrawToken to true.
     */
-    function withdrawFunds(bool _withdrawToken, address _targetToken) public onlyOwner returns(bool _success){
+    function withdrawFunds(bool _withdrawToken, address _targetToken) public onlyRole("Admin") returns(bool _success){
 
         if(_withdrawToken){
             require(_targetToken != address(0), "Error: Invalid token address");
@@ -86,5 +106,7 @@ contract RandomNumber is VRFConsumerBase, Ownable{
         if(address(this).balance > 0){
             (_success,) = payable(msg.sender).call{ value: address(this).balance }("");
         }
+
+        emit withdrawContractFunds(block.timestamp, _success);
     }
 }
--- test/randomNumber.test.js
+++ test/randomNumber.test.js
@@ -3,13 +3,21 @@ const { ethers } = require("hardhat");
 const linkAbi = require("./ContractJson/Link.json");
 
 describe("RandomNumber", async ()=> {
-    let RandomNumber, randomNumber, VrfCoordinator, vrfCoordinator, link, deployer, per1, perLink;
+    let RandomNumber, randomNumber, VrfCoordinator, vrfCoordinator, link, owner1, owner2, user1, userWithLink;
 
     before(async ()=> {
         await hre.network.provider.request({ 
             method: "hardhat_impersonateAccount",
             params: ["0xd8f772840d9bf2141ec64c1b17127d14663c3226"],
         });
+
+        userWithLink = await ethers.getSigner("0xd8f772840d9bf2141ec64c1b17127d14663c3226");
+        [owner1, owner2, user1] = await ethers.getSigners();
+
+        await network.provider.send("hardhat_setBalance", [
+            userWithLink.address,
+            ethers.utils.formatBytes32String("5000000000000000000"),
+        ]);
 
         link = await new ethers.Contract( "0x404460C6A5EdE2D891e8297795264fDe62ADBB75" , linkAbi);
 
@@ -19,81 +27,79 @@ describe("RandomNumber", async ()=> {
         RandomNumber = await ethers.getContractFactory("RandomNumber");
         randomNumber = await RandomNumber.deploy(
             vrfCoordinator.address,
-            "0x404460C6A5EdE2D891e8297795264fDe62ADBB75"
+            "0x404460C6A5EdE2D891e8297795264fDe62ADBB75",
+            owner2.address
         );
-
-        perLink = await ethers.getSigner("0xd8f772840d9bf2141ec64c1b17127d14663c3226");
-        [deployer, per1] = await ethers.getSigners();
-
-        await network.provider.send("hardhat_setBalance", [
-            perLink.address,
-            ethers.utils.formatBytes32String("5000000000000000000"),
-        ]);
     });
 
     describe("Link balance in the contract", async ()=> {
 
         it("Shouldn't have balance", async ()=> {
-            expect(await link.connect(deployer).balanceOf(randomNumber.address)).to.equal(0);
+            expect(await link.connect(owner1).balanceOf(randomNumber.address)).to.equal(0);
         });
 
         it("No link does not work", async ()=> {
-            await expect(randomNumber.connect(deployer).getRandomNumber(
+            await expect(randomNumber.connect(owner2).getRandomNumber(
             )).to.be.revertedWith("Not enough LINK - fill contract with faucet");
         });
 
         it("Should have balance", async ()=> {
-            await link.connect(perLink).transfer(
+            await link.connect(userWithLink).transfer(
                 randomNumber.address, 
                 ethers.utils.parseEther("1000")
             );
 
-            expect(await link.connect(deployer).balanceOf(
+            expect(await link.connect(owner1).balanceOf(
                 randomNumber.address
             )).to.equal(ethers.utils.parseEther("1000"));
         });
     });
 
     describe("Security", async ()=> {
-        it("Only the owner can execute the functions", async ()=> {
-            await expect(randomNumber.connect(per1).getRandomNumber(
-            )).to.be.revertedWith("Ownable: caller is not the owner");
+        it("Only user with appropriate roles can execute the functions", async ()=> {
 
-            await expect(randomNumber.connect(per1).setUntil(
-                5
-            )).to.be.revertedWith("Ownable: caller is not the owner");
+            await expect(randomNumber.connect(owner1).getRandomNumber()).to.be.revertedWith(
+                "AccessControl: account 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 is missing role 0x436f6e7472616374000000000000000000000000000000000000000000000000"
+            );
 
-            await expect(randomNumber.connect(per1).withdrawFunds(
-                true,
-                link.address
-            )).to.be.revertedWith("Ownable: caller is not the owner");
+            await expect(randomNumber.connect(owner2).setUntil(5)).to.be.revertedWith(
+                "AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x41646d696e000000000000000000000000000000000000000000000000000000"
+            );
+
+            await expect(randomNumber.connect(owner2).withdrawFunds(true, link.address)).to.be.revertedWith(
+                "AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x41646d696e000000000000000000000000000000000000000000000000000000"
+            );
+
+            await expect(randomNumber.connect(user1).withdrawFunds(true, link.address)).to.be.revertedWith(
+                "AccessControl: account 0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc is missing role 0x41646d696e000000000000000000000000000000000000000000000000000000"
+            );
         });
     });
 
     describe("Get value", async ()=> {
         it("Start value must be 0", async ()=> {
-            expect(await randomNumber.connect(deployer).until()).to.equal(1);
-            expect(await randomNumber.connect(deployer).randomResult()).to.equal(0);
+            expect(await randomNumber.connect(owner1).until()).to.equal(999999);
+            expect(await randomNumber.connect(owner1).randomResult()).to.equal(0);
         });
 
         it("The 'Until' value must be modified", async ()=> {
-            await randomNumber.connect(deployer).setUntil(20);
-            expect(await randomNumber.connect(deployer).until()).to.equal(20);
+            await randomNumber.connect(owner1).setUntil(999998);
+            expect(await randomNumber.connect(owner1).until()).to.equal(999998);
         });
 
         it("Should return a random number in the given range", async ()=> {
-            await randomNumber.connect(deployer).getRandomNumber();
-            let requestId = await randomNumber.connect(deployer).lastRequestId();
+            await randomNumber.connect(owner2).getRandomNumber();
+            let requestId = await randomNumber.connect(owner1).lastRequestId();
 
-            await vrfCoordinator.connect(deployer).callBackWithRandomness(
+            await vrfCoordinator.connect(owner1).callBackWithRandomness(
                 requestId,
                 "777",
                 randomNumber.address
             );
-            let value = await randomNumber.connect(deployer).randomResult();
+            let value = await randomNumber.connect(owner1).randomResult();
 
             assert(value > 0);
-            assert(value <= 20);
+            assert(value <= 999998);
         });
     });
 
@@ -110,20 +116,20 @@ describe("RandomNumber", async ()=> {
         });
 
         it("Error: Token address should be valid", async ()=> {
-            await expect(randomNumber.connect(deployer).withdrawFunds(
+            await expect(randomNumber.connect(owner1).withdrawFunds(
                 true,
                 "0x0000000000000000000000000000000000000000"
             )).to.be.revertedWith("Error: Invalid token address");
         });
 
         it("Withdraw tokens and BNB", async ()=> {
-            await randomNumber.connect(deployer).withdrawFunds(
+            await randomNumber.connect(owner1).withdrawFunds(
                 true,
                 link.address
             )
 
             expect(await ethers.provider.getBalance(randomNumber.address)).to.equal(0);
-            expect(await link.connect(deployer).balanceOf(
+            expect(await link.connect(owner1).balanceOf(
                 randomNumber.address
             )).to.equal(0);
         });
